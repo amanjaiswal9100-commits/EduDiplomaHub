4a7d41f76383be493697a2375ff30d1e9b2c7d8e
Aman Jaiswal <amanjaiswal9100@gmail.com>
2026-01-13 22:05:29 +0530

    Create fresh admin2 superuser for access

--- hub/apps.py
+++ hub/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_admin2(sender, **kwargs):
+    from django.contrib.auth.models import User
+
+    if not User.objects.filter(username="admin2").exists():
+        User.objects.create_superuser(
+            username="admin2",
+            email="edudiplomahub@gmail.com",
+            password="Admin@123"
+        )
 
 class HubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hub'
+
+    def ready(self):
+        post_migrate.connect(create_admin2, sender=self)
