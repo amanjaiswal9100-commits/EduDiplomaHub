0f8656f60c5f56bddd70465e3576b098d5cb7ef5
Aman Jaiswal <amanjaiswal9100@gmail.com>
2026-01-13 20:59:44 +0530

    Add temporary auto superuser and fix settings for Render

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'hub',
+    'hub.apps.HubConfig',
 ]
 
 # --------------------------------------------------
--- hub/apps.py
+++ hub/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class HubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hub'
+
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="edudiplomahub@gmail.com",
+                password="jaisu@123321"
+            )
