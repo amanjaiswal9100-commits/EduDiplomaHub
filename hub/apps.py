c38f478901fdb92c290e03cd051da46be92a5d68
Aman Jaiswal <amanjaiswal9100@gmail.com>
2026-01-13 21:45:56 +0530

    Temporary admin password reset via post_migrate

--- hub/apps.py
+++ hub/apps.py
@@ -1,5 +1,25 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def create_admin(sender, **kwargs):
+    from django.contrib.auth.models import User
+
+    admin, created = User.objects.get_or_create(
+        username="admin",
+        defaults={
+            "email": "edudiplomahub@gmail.com",
+            "is_staff": True,
+            "is_superuser": True,
+        }
+    )
+
+    # 🔑 FORCE PASSWORD SET (SAFE HERE)
+    admin.set_password("Admin@123")
+    admin.save()
 
 class HubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hub'
+
+    def ready(self):
+        post_migrate.connect(create_admin, sender=self)
